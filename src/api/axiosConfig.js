4d2333930f52f30773b53f2c268df10f3a0fea40
JonathanF1210 <jonathanf1210@gmail.com>
2023-05-23 14:00:13 -0500

    added header, carousel, trailer and review functionality

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://adde-2600-6c56-6000-71d-e5ee-23e-807e-c19.ngrok-free.app/',
+    baseURL:'https://3176-2600-6c56-6000-71d-db6-cbe3-25af-16d8.ngrok-free.app',
     headers: {"ngrok-skip-browser-warning": "true"}
 
 });
